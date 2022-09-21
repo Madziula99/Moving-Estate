8c71ce30af07659c8fd31896dc0f8c77c8cde679
Yuliya Mironava <y.mironava@godeltech.com>
2022-09-21 14:02:07 +0200

    Feat Header in components folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Header.module.css";
+import logo from "../pictures/logo.png";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className={classes.header_line}></div>
+        <div className={classes.header_wrapper}>
+          <div className={classes.header}>
+            <img className={classes.header_logo} src={logo} alt="logo" />
+            <h2>This.text</h2>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Header from "../components/Header.jsx";
 
 class ComponentGallery extends React.Component {
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
 }
 
 export default ComponentGallery;
