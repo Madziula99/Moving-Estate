ffa06b4778c9609c19e0e51657da9c6b8d4619e9
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-21 12:03:58 +0200

    Fix files extensions and structure

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import ComponentGallery from './components/ComponentGallery';
+import ComponentGallery from './pages/ComponentGallery.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
