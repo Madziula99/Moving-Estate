3735cbc7a1ae967ca3563a7bb926787e026bb569
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-21 22:43:44 +0200

    Add ContactListItems to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import styles from './Footer.module.css';
 
+import ContactInfoItem from "../ContactInfoItem/ContactInfoItem.jsx";
+
 import {ReactComponent as Logo} from './logo.svg';
 
 class Footer extends React.Component {
@@ -9,8 +11,13 @@ class Footer extends React.Component {
     return <footer>
       <div className={styles.wrapper}>
         <Logo stroke="#DF434A" className={styles.footer_logo}/>
-        <div>
-          <span className={styles.contact_title}>Contact</span>
+        <div className={styles.footer_contact}>
+          <span className={styles.contact_title}>Contact Info</span>
+          <div className={styles.contact_info}>
+            <ContactInfoItem className={styles.contact_info_item} type="address">24th Street, New York, USA</ContactInfoItem>
+            <ContactInfoItem className={styles.contact_info_item} type="phone">+0 123-456-7890</ContactInfoItem>
+            <ContactInfoItem className={styles.contact_info_item} type="email">info@example.com</ContactInfoItem>
+          </div>
         </div>
       </div>
 
