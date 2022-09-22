96469bee218951c8c524c597e0a52df79f18016e
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-22 13:53:11 +0200

    Change css styles for Footer and ContactInfoItem

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ContactInfoItem from "../components/ContactInfoItem/ContactInfoItem.jsx";
 
 import Footer from "../components/Footer/Footer.jsx";
 
@@ -8,6 +9,11 @@ class ComponentGallery extends React.Component {
             <>
                 <h2>Footer</h2>
                 <Footer />
+
+
+                <h2>Contact Info Item</h2>
+                <ContactInfoItem type="phone" isFooter={false}>+0 123-456-7890</ContactInfoItem>
+                <ContactInfoItem type="email" isFooter={false}>info@example.com</ContactInfoItem>
 
             </>
         )
