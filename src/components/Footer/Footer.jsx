76cef53f05ba988eade1399fae7a31240ec2cb90
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-22 13:53:11 +0200

    Change css styles for Footer and ContactInfoItem

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from './Footer.module.css';
 import ContactInfoItem from "../ContactInfoItem/ContactInfoItem.jsx";
+<<<<<<< HEAD
 import Logo from "../Logo/Logo.jsx";
+=======
+import {ReactComponent as Logo} from './logo.svg';
+>>>>>>> 96469be (Change css styles for Footer and ContactInfoItem)
 
 class Footer extends React.Component {
   render() {
     return <footer>
       <div className={styles.wrapper}>
+<<<<<<< HEAD
         <Logo isFooter={true} />
+=======
+        <Logo className={styles.footer_logo}/>
+>>>>>>> 96469be (Change css styles for Footer and ContactInfoItem)
         <div className={styles.footer_contact}>
           Contact Info
           <div className={styles.contact_info}>
--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 import Header from "../components/Header/Header.jsx";
 import Subtitle from "../components/Subtitle/Subtitle.jsx";
 import Description from "../components/Description/Description";
@@ -31,6 +32,27 @@ class ComponentGallery extends React.Component {
       </>
     );
   }
+=======
+import ContactInfoItem from "../components/ContactInfoItem/ContactInfoItem.jsx";
+
+import Footer from "../components/Footer/Footer.jsx";
+
+class ComponentGallery extends React.Component {
+    render() {
+        return (
+            <>
+                <h2>Footer</h2>
+                <Footer />
+
+
+                <h2>Contact Info Item</h2>
+                <ContactInfoItem type="phone" isFooter={false}>+0 123-456-7890</ContactInfoItem>
+                <ContactInfoItem type="email" isFooter={false}>info@example.com</ContactInfoItem>
+
+            </>
+        )
+    }
+>>>>>>> 96469be (Change css styles for Footer and ContactInfoItem)
 }
 
 export default ComponentGallery;
