0a9744529719dde4f3d4c69340c4c067266c98d0
Rescor <rescorent@gmail.com>
2022-09-22 14:24:03 +0200

    Added a description component

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Description from "../Components/Description";
 
 class ComponentGallery extends React.Component {
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+
+        <Description>This is a description test component. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, facere natus nostrum placeat praesentium quas recusandae repellendus vero. Beatae consectetur doloribus eveniet facilis fuga magnam necessitatibus nesciunt porro quam soluta!</Description>
+
+      </>
+    )
+  }
 }
 
 export default ComponentGallery;
