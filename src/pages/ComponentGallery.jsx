c1acfcdd98c5377af41037dae045c956fd62db09
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-22 16:59:32 +0200

    Add Button component

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "../components/Header/Header.jsx";
 import Subtitle from "../components/Subtitle/Subtitle.jsx";
+import Button from "../components/Button/Button.jsx";
+import {ReactComponent as ListModeIcon} from './listMode.svg';
+import {ReactComponent as GridModeIcon} from './gridMode.svg';
 
 class ComponentGallery extends React.Component {
   render() {
