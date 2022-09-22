7e2258bb18f6ee546c9b87fbb4d7d29c72fd629e
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-22 11:59:09 +0200

    Fix ContactInfoItem to be reusable for AgentInfo

--- src/components/ContactInfoItem/ContactInfoItem.jsx
+++ src/components/ContactInfoItem/ContactInfoItem.jsx
@@ -4,10 +4,15 @@ import styles from './ContactInfoItem.module.css';
 class ContactInfoItem extends React.Component {
   render() {
     const type = this.props.type;
+
+    const classes = [styles.icon];
+    this.props.isFooter && classes.push(styles.footer);
+
     let prefix = null;
     type !== "address" && (prefix = (type === "email" ? "mailto:" : "tel:"))
+
     return <div className={styles.contact_info_item}>
-      <div className={styles.icon}>
+      <div className={classes.join(' ')}>
         <img src={require(`./${type}.svg`)} alt="" />
       </div>
       <a className={styles.contact_link} href={prefix && prefix + this.props.children}>
