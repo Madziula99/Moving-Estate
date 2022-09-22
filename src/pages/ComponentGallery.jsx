d57b6c0607aeec9b25d9a4ff500f8b17c73a19f5
Rescor <rescorent@gmail.com>
2022-09-22 12:05:09 +0200

    Refactor subtitle module

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Subtitle from "../components/Subtitle";
 class ComponentGallery extends React.Component {
     render() {
         return (
             <>
+                <Subtitle>test</Subtitle>
             </>
         )
     }
