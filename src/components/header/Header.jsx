b1143bab92698baac6a6568bc294339e2baee146
Rescor <rescorent@gmail.com>
2022-09-23 12:30:29 +0200

    Fix a path in Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Header.module.css";
-import Logo from "../logo/Logo.jsx";
+import Logo from "../Logo/Logo.jsx";
 
 class Header extends React.Component {
   render() {
