14e34c5e340a9a4f9d57b7088919c9e30f8745c8
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-23 12:37:00 +0200

    Fix Button rendering in Pagination

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -5,39 +5,79 @@ import {ReactComponent as GridModeIcon} from './gridMode.svg';
 import Pagination from "../components/Pagination/Pagination.jsx";
 
 class ComponentGallery extends React.Component {
-    render() {
-        const num = 5;
-        const pages = [];
-        for (let i = 1; i <= num; i++) {
-            const position = ( (i === 1) && "left" ) || ( (i === num) && "right" );
-            pages.push(<Button type="page" position={position} id={i} key={i}>{i}</Button>);
-        }
-        return (
-            <>
-                <h2>&lt;Button&gt;</h2>
-                <div style={{display: "flex"}}>
-                    <Button type="page" position="standalone" onChange={() => console.log(`Page changed to 3`)}>&lt;</Button>
-                    <div style={{margin: "0 1rem"}}>
-                        {pages}
-                    </div>
-                    <Button type="page" position="standalone" onChange={() => console.log(`Page changed to 3`)}>&gt;</Button>
-                </div>
-                <hr />
-                <Button type="view_mode" position="left" onChange={() => console.log(`Toggle view`)}>
-                    <GridModeIcon />
-                </Button>
-                <Button type="view_mode" position="right" onChange={() => console.log(`Toggle view`)}>
-                    <ListModeIcon />
-                </Button>
+  render() {
+    return (
+      <>
+        <h2>&lt;Button&gt;</h2>
+        <div style={{margin: "1rem"}}>
+          <Button
+            type="page"
+            roundedLeft
+            roundedRight
+            onClick={() => console.log(`Page change: Previous page`)}
+          >
+            &lt;
+          </Button>
+          <br />
+          <Button
+            type="page"
+            roundedLeft
+            roundedRight
+            onClick={() => console.log(`Page change: Next page`)}
+          >
+            &gt;
+          </Button>
+          <br />
+          <Button
+            type="page"
+            roundedLeft
+            isFocused
+            onClick={() => console.log(`Page change: Page 1`)}
+          >
+            1
+          </Button>
+          <br />
+          <Button
+            type="page"
+            onClick={() => console.log(`Page change: Page 2`)}
+          >
+            2
+          </Button>
+          <br />
+          <Button
+            type="page"
+            roundedRight
+            onClick={() => console.log(`Page change: Page 3`)}
+          >
+            3
+          </Button>
+          <br />
+          <Button
+            type="view_mode"
+            roundedLeft
+            isFocused
+            onClick={() => console.log(`Toggle view: Grid Mode`)}
+          >
+            <GridModeIcon />
+          </Button>
+          <br />
+          <Button
+            type="view_mode"
+            roundedRight
+            onClick={() => console.log(`Toggle view: List Mode`)}
+          >
+            <ListModeIcon />
+          </Button>
+        </div>
 
 
-                <h2>&lt;Pagination&gt;</h2>
-                <Pagination pages={7} page={1} onChange={page => console.log(`Page changed to ${page}`)} />
-                <Pagination pages={7} page={3} onChange={page => console.log(`Page changed to ${page}`)} />
-                <Pagination pages={7} page={7} onChange={page => console.log(`Page changed to ${page}`)} />
-            </>
-        )
-    }
+        <h2>&lt;Pagination&gt;</h2>
+        <Pagination pages={7} page={1} onChange={page => console.log(`Page changed to ${page}`)} />
+        <Pagination pages={7} page={3} onChange={page => console.log(`Page changed to ${page}`)} />
+        <Pagination pages={7} page={7} onChange={page => console.log(`Page changed to ${page}`)} />
+      </>
+    )
+  }
 }
 
 export default ComponentGallery;
