26587b9c9e3ba29e72314bcdfd739882549329cd
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-23 13:19:28 +0200

    Add Logo component to Footer

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import ContactInfoItem from "../components/ContactInfoItem/ContactInfoItem.jsx";
 import Header from "../components/Header/Header.jsx";
 import Subtitle from "../components/Subtitle/Subtitle.jsx";
+import ContactInfoItem from "../components/ContactInfoItem/ContactInfoItem.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
 class ComponentGallery extends React.Component {
@@ -15,6 +15,16 @@ class ComponentGallery extends React.Component {
         {/* prettier-ignore */}
 
         <Subtitle>Subtitle Component</Subtitle>
+
+        <h2>&lt;ContactInfoItem&gt;</h2>
+        <ContactInfoItem type="phone" isFooter={false}>+0 123-456-7890</ContactInfoItem>
+        <ContactInfoItem type="email" isFooter={false}>info@example.com</ContactInfoItem>
+        <ContactInfoItem type="address" isFooter={true}>24th Street, New York, USA</ContactInfoItem>
+        <ContactInfoItem type="phone" isFooter={true}>+0 123-456-7890</ContactInfoItem>
+        <ContactInfoItem type="email" isFooter={true}>info@example.com</ContactInfoItem>
+
+        <h2>&lt;Footer&gt;</h2>
+        <Footer />
 
       </>
     );
