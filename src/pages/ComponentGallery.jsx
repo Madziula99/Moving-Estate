54733b4068df9d39f9ccfcd151cf496a64148739
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-26 12:33:30 +0200

    Extract methods in Button and Pagination for more readable code

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -11,68 +11,6 @@ class ComponentGallery extends React.Component {
   render() {
     return (
       <>
-        <h2>&lt;Button&gt;</h2>
-        <div style={{margin: "1rem"}}>
-          <Button
-            type="page"
-            roundedLeft
-            roundedRight
-            onClick={() => console.log(`Page change: Previous page`)}
-          >
-            &lt;
-          </Button>
-          <br />
-          <Button
-            type="page"
-            roundedLeft
-            roundedRight
-            onClick={() => console.log(`Page change: Next page`)}
-          >
-            &gt;
-          </Button>
-          <br />
-          <Button
-            type="page"
-            roundedLeft
-            isFocused
-            onClick={() => console.log(`Page change: Page 1`)}
-          >
-            1
-          </Button>
-          <br />
-          <Button
-            type="page"
-            onClick={() => console.log(`Page change: Page 2`)}
-          >
-            2
-          </Button>
-          <br />
-          <Button
-            type="page"
-            roundedRight
-            onClick={() => console.log(`Page change: Page 3`)}
-          >
-            3
-          </Button>
-          <br />
-          <Button
-            type="view_mode"
-            roundedLeft
-            isFocused
-            onClick={() => console.log(`Toggle view: Grid Mode`)}
-          >
-            <GridModeIcon />
-          </Button>
-          <br />
-          <Button
-            type="view_mode"
-            roundedRight
-            onClick={() => console.log(`Toggle view: List Mode`)}
-          >
-            <ListModeIcon />
-          </Button>
-        </div>
-
         <Header>PROPERTIES</Header>
 
         {/* prettier-ignore */}
@@ -83,10 +21,6 @@ class ComponentGallery extends React.Component {
 
         <Subtitle>Subtitle Component</Subtitle>
 
-        <h2>&lt;Pagination&gt;</h2>
-        <Pagination pages={7} page={1} onChange={page => console.log(`Page changed to ${page}`)} />
-        <Pagination pages={7} page={3} onChange={page => console.log(`Page changed to ${page}`)} />
-        <Pagination pages={7} page={7} onChange={page => console.log(`Page changed to ${page}`)} />
         <h2>&lt;Button&gt;</h2>
         <div style={{margin: "1rem"}}>
           <Button
