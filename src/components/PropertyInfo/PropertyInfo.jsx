faf3eb28f9c6b6c4054102e7c39b57636f68ec07
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-09-26 13:19:46 +0200

    Add centered-style prop presentation

--- src/components/PropertyInfo/PropertyInfo.jsx
+++ src/components/PropertyInfo/PropertyInfo.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { ReactComponent as ApartmentIcon } from './apartment.svg';
-import { ReactComponent as TownhouseIcon } from './townhouse.svg';
-import { ReactComponent as AreaIcon } from './area.svg';
-import { ReactComponent as BedsIcon } from './beds.svg';
-import { ReactComponent as BathsIcon } from './baths.svg';
+import { ReactComponent as ApartmentIcon } from "./apartment.svg";
+import { ReactComponent as TownhouseIcon } from "./townhouse.svg";
+import { ReactComponent as AreaIcon } from "./area.svg";
+import { ReactComponent as BedsIcon } from "./beds.svg";
+import { ReactComponent as BathsIcon } from "./baths.svg";
 import styles from "./PropertyInfo.module.css";
 
 class PropertyInfo extends React.Component {
   generateTypeIcon() {
     switch (this.props.type) {
-      case 'apartment': return ApartmentIcon;
-      case 'townhouse': return TownhouseIcon;
+      case "apartment": return ApartmentIcon;
+      case "townhouse": return TownhouseIcon;
       default: return null;
     }
   }
@@ -26,15 +26,24 @@ class PropertyInfo extends React.Component {
     }
   }
 
+  addClassName() {
+    const classes = [`${styles.list}`];
+    
+    if (this.props.style === "centered") { classes.push(`${styles.centered}`); }
+
+    return classes.join(" ");
+  }
+
+
   render() {
     const availableProps = [];
 
-    if (this.props.type) availableProps.push({ prop: this.props.type, icon: this.generateTypeIcon() });
+    if (this.props.type) availableProps.push({ prop: this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1), icon: this.generateTypeIcon() });
     if (this.props.area) availableProps.push({ prop: `${this.props.area}ft²`, icon: AreaIcon });
     if (this.props.beds) availableProps.push({ prop: this.props.beds, icon: BedsIcon });
     if (this.props.baths) availableProps.push({ prop: this.props.baths, icon: BathsIcon });
 
-    return <ul className={styles.list}>
+    return <ul className={this.addClassName()}>
       {availableProps.map((item, i) => {
         return <li key={i} className={styles.list_item}>
           {this.showIcon(item.icon)}
