a850af08485e4f69f7bbe4d8614ba7fe2e3f1fd0
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-09-27 14:27:32 +0200

    Fix styles and refactor code

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import ComponentGallery from './pages/ComponentGallery.jsx';
+import ComponentGallery from "./pages/ComponentGallery.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ComponentGallery />
