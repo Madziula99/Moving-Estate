ecf2871f02bf654d6b98728d1c1653a1ed146a8c
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-27 13:24:59 +0200

    Add ViewModeToggle component

--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -6,6 +6,7 @@ import Footer from "../components/Footer/Footer.jsx";
 import ContactInfoItem from "../components/ContactInfoItem/ContactInfoItem.jsx";
 import PropertyInfo from "../components/PropertyInfo/PropertyInfo.jsx";
 import Pagination from "../components/Pagination/Pagination.jsx";
+import ViewModeToggle from "../components/ViewModeToggle/ViewModeToggle.jsx";
 
 class ComponentGallery extends React.Component {
   render() {
@@ -42,6 +43,10 @@ class ComponentGallery extends React.Component {
 
         <h2>&lt;Pagination&gt;</h2>
         <Pagination pages={7} page={3} onChange={page => console.log(`Page changed to ${page}`)} />
+
+        <h2>&lt;ViewModeToggle&gt;</h2>
+        <ViewModeToggle mode="grid" onChange={mode => console.log(`View mode changed to ${mode}`)} />
+        <ViewModeToggle mode="list" onChange={mode => console.log(`View mode changed to ${mode}`)} />
       </>
     )
   }
