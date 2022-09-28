1839e785b6539de88999d3ec64fe270d3637cdb3
Rescor <rescorent@gmail.com>
2022-09-28 12:03:49 +0200

    Code refactoring

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -5,8 +5,8 @@ import styles from "./Title.module.css";
 class Title extends React.Component {
   render() {
     return <>
-      <h2 className={styles.apartment_title}>{this.props.name}</h2>
-      <span className={styles.apartment_city_area}>{this.props.location.join(", ")}</span>
+      <h2 className={styles.property_title}>{this.props.name}</h2>
+      <span className={styles.property_city_area}>{this.props.location.join(", ")}</span>
       <PropertyInfo
         type={this.props.type}
         area={this.props.area}
