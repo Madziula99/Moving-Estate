6c32b4421591dc9cc72ea9391d16e1f1ce8f9fa0
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-09-28 15:52:01 +0200

    Add clickHandler to btn and change some styles

--- src/components/FloorPlans/FloorPlans.jsx
+++ src/components/FloorPlans/FloorPlans.jsx
@@ -3,13 +3,29 @@ import Subtitle from "../Subtitle/Subtitle.jsx";
 import classes from "./FloorPlans.module.css";
 
 class FloorPlans extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentBtn: this.props.plans[0],
+    };
+  }
+
+  showImage(e) {
+    this.setState({ currentBtn: { name: e.target.innerText } });
+  }
+
   render() {
     return (
       <section>
         <Subtitle>FLOOR PLANS</Subtitle>
         <div className={classes.floor_plans}>
-         <button className={classes.plan_btn}>{this.props.plans[1].name}</button> 
-          <img className={classes.floor_img} alt={this.props.plans[1].name} src={this.props.plans[1].url} />          
+          {this.props.plans.map((plan, i) => {
+            return <React.Fragment key={`${i}-${plan.name}`}>
+              <button className={classes.plan_btn} onClick={(e) => this.showImage(e)}>{plan.name}</button> 
+            </React.Fragment>
+          })}
+          <img className={classes.floor_img} src={this.props.plans[1].url} alt={this.props.plans[1].url} />       
         </div>        
       </section>
     );
--- src/pages/ComponentGallery.jsx
+++ src/pages/ComponentGallery.jsx
@@ -117,7 +117,7 @@ class ComponentGallery extends React.Component {
         <h2>&lt;FloorPlans&gt;</h2>
         <FloorPlans plans={[
           { name: "Ground Floor", url: "https://www.purelocations.com.au/wp-content/uploads/2022/05/Analise-Mornington_87201.jpg?v=1653962316" },
-          { name: "1st Floor", url: "https://www.purelocations.com.au/wp-content/uploads/2022/05/Analise-Mornington_87201.jpg?v=1653962316" },
+          { name: "1st Floor", url: "https://wpmedia.roomsketcher.com/content/uploads/2022/01/06145219/Floor-plan-with-dimensions.jpg" },
           ]}
         />
 
