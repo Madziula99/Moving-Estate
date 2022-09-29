bea4a30fe27c62da3331a4df69df18a989784232
Magdalena Stopka <m.stopka@godeltech.com>
2022-09-29 13:56:59 +0200

    Add scroll to top of PropertyList on page change and modify ViewModeToggle styles

--- src/components/PropertyList/PropertyList.jsx
+++ src/components/PropertyList/PropertyList.jsx
@@ -8,24 +8,26 @@ class PropertyList extends React.Component {
   constructor(props) {
     super(props);
 
+    this.propertyListRef = React.createRef();
+
     let pageSize = this.props.defaultView === "grid" ? 12 : 8;
     let pages = Math.ceil(this.props.properties.length / pageSize);
-    let list = [];
+    let propertiesList = [];
     for (let i = 0; i < this.props.properties.length; i += pageSize) {
-      list.push(this.props.properties.slice(i, i + pageSize));
+      propertiesList.push(this.props.properties.slice(i, i + pageSize));
     }
 
     this.state = {
       currentMode: this.props.defaultView,
       pageSize: pageSize,
-      propertiesList: list,
+      propertiesList: propertiesList,
       pages: pages,
       currentPage: 1
-    }
+    };
   }
 
   toggleViewMode(mode) {
-    if(mode === this.props.currentMode) return;
+    if (mode === this.props.currentMode) return;
     const newPageSize = (mode === "grid") ? 12 : 8;
     const pages = Math.ceil(this.props.properties.length / newPageSize);
     const page = (this.state.currentPage > pages) ? pages : this.state.currentPage;
@@ -33,30 +35,34 @@ class PropertyList extends React.Component {
     for (let i = 0; i < this.props.properties.length; i += newPageSize) {
       propertiesList.push(this.props.properties.slice(i, i + newPageSize));
     }
+
     this.setState({
-      pageSize: newPageSize,
       currentMode: mode,
+      pageSize: newPageSize,
+      propertiesList: propertiesList,
       pages: pages,
-      currentPage: page,
-      propertiesList: propertiesList
-    })
+      currentPage: page
+    });
   }
 
-  switchPage(page) {
-    if(page === this.props.currentPage) return;
+  changePage(page) {
+    if (page === this.props.currentPage) return;
+    this.propertyListRef.current.scrollIntoView({ behavior: 'smooth'});
+
     this.setState({
       currentPage: page
-    })
+    });
   }
 
   addClasses() {
     const classes = [styles.properties_wrapper];
     this.state.currentMode === "grid" && classes.push(styles.grid_view_mode);
+
     return classes.join(" ");
   }
 
   render() {
-    return <>
+    return <div ref={this.propertyListRef}>
       <ViewModeToggle
         mode={this.state.currentMode}
         onChange={mode => this.toggleViewMode(mode)}
@@ -76,9 +82,9 @@ class PropertyList extends React.Component {
       <Pagination
         pages={this.state.pages}
         page={this.state.currentPage}
-        onChange={page => this.switchPage(page)}
+        onChange={page => this.changePage(page)}
       />
-    </>
+    </div>
   }
 }
 
