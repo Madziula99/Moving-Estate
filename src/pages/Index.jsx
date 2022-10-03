b321223cade8c3441b2ada9f8ed5a2da6f9f7df7
Yuliya Mironava <y.mironava@godeltech.com>
2022-10-03 18:04:06 +0200

    Add correct data from json

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,37 +1,35 @@
 import React from "react";
-import { NavLink, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 import PropertyList from "../components/PropertyList/PropertyList";
+import properties from "../data.json";
 
 class Index extends React.Component {
   render() {
-    const property = {
-      title: "Dream Apartment for Young Family",
-      location: ["Pasadena", "California"],
-      image: "https://i.picsum.photos/id/323/300/300.jpg?hmac=x5Uyo5A2WJUbsbAXSto7yT2T3WBX2rfCrDk_7dY8kzU",
-      description: "Lorem ipsum...",
-      type: "apartment",
-      mode: "sale",
-      price: 100500,
-      area: 1320,
-      bedrooms: 2,
-      bathrooms: 2
-    }
+    const newArr = properties.map(property => {
+      return {
+        id:property.id,
+        title: property.title,
+        location: property.location,
+        image: property.images[0],
+        description: property.description,
+        type: property.type,
+        mode: property.mode,
+        price: property.price,
+        area: property.area,
+        bedrooms: property.bedrooms,
+        bathrooms: property.bathrooms,
+      }
+    })    
 
     return <>
+      <Header>PROPERTIES</Header>
       <PropertyList
-          defaultView="grid"
-          properties={[
-            {...property, id: 'A001'}, {...property, id: 'A002'}, {...property, id: 'A003'},
-            {...property, id: 'A004'}, {...property, id: 'A005'}, {...property, id: 'A006'},
-            {...property, id: 'A007'}, {...property, id: 'A008'}, {...property, id: 'A009'},
-            {...property, id: 'A010'}, {...property, id: 'A011'}, {...property, id: 'A012'},
-            {...property, id: 'A013'}, {...property, id: 'A014'}, {...property, id: 'A015'},
-            {...property, id: 'A016'}, {...property, id: 'A017'}, {...property, id: 'A018'},
-            {...property, id: 'A019'}, {...property, id: 'A020'}, {...property, id: 'A021'}
-        ]}
-        
-        //TODO: ul->and NavLink
-        />
+        defaultView="grid"
+        properties={newArr}
+      />
+      <Footer />
     </>
   }
 }
