1890f2bd5b777193aed174b8298875e95e7bbffc
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-10-03 14:10:13 +0200

    Make arrows visible on hover the top image and refactor code

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./Gallery.css";
 
 class Gallery extends React.Component {
   render() {
-    return <Carousel transitionTime="500" infiniteLoop showIndicators={false} showStatus={false}>
+    return <Carousel transitionTime="500" showIndicators={false} showStatus={false}>
       {this.props.images.map((image) => {
         return <img src={image} alt="Gallery slide" key={image} />
       })}
