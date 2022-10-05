e526b3fa3f7b09adca70630c8a47823d8eb9881b
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-10-05 12:22:03 +0200

    Change css names and add their classes

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -34,8 +34,8 @@ class PropertyCard extends React.Component {
         <img className={styles.property_card_image} src={this.props.image} alt="Accommodation" />
         <div className={this.addWrapperClassName()}>
           <h3 className={styles.property_card_name}>{this.props.title}</h3>
-          <span>{this.props.location.join(", ")}</span>
-          <p>{this.props.description}</p>
+          <span className={styles.property_card_location}>{this.props.location.join(", ")}</span>
+          <p className={styles.property_card_description}>{this.props.description}</p>
           <PropertyInfo
             area={1932}
             beds={4}
