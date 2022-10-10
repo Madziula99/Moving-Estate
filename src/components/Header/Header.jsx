1a4901694716d72117a731ce78d71c50716415c1
Yuliya Mironava <y.mironava@godeltech.com>
2022-10-10 14:10:49 +0200

    Deleted withRouter (it is not necessary there)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {NavLink, withRouter } from "react-router-dom"
+import {NavLink } from "react-router-dom"
 import Logo from "../Logo/Logo.jsx";
 import classes from "./Header.module.css";
 
@@ -16,4 +16,4 @@ class Header extends React.Component {
   }
 }
 
-export default withRouter(Header);
+export default Header;
