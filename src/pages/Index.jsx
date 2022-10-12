4e7b0a42a3c194ded50eab047d8512cb1db09dc7
Yuliya Mironava <y.mironava@godeltech.com>
2022-10-12 23:01:33 +0200

    Refactor code

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,57 +5,45 @@ import PropertyList from "../components/PropertyList/PropertyList.jsx";
 import { Sidebar } from "../components/Sidebar/Sidebar.jsx";
 import properties from "../data.json";
 
-const necessaryProperties = properties.map(property => {
-  return {
-    id: property.id,
-    title: property.title,
-    location: property.location,
-    image: property.images[0],
-    description: property.description,
-    type: property.type,
-    mode: property.mode,
-    price: property.price,
-    area: property.area,
-    bedrooms: property.bedrooms,
-    bathrooms: property.bathrooms
-  }
-});
-
 class Index extends React.Component {
-  createOptionsObject() {
-    const options = {
-      type: [],
-      minArea: 0,
-      maxArea: 0,
-      status: [],
-      bedrooms: [],
-      bathrooms: [],
-      location: [],
-      minPrice: 0,
-      maxPrice: 0,
-      minYearBuilt: 0,
-    };
-
-    const types = [...new Set(necessaryProperties.map((property => property.type)))];
-    const area = [...new Set(necessaryProperties.map((property => property.area)))];
-    const statuses = [...new Set(necessaryProperties.map((property => property.mode)))];
-    const bedrooms = [...new Set(necessaryProperties.map((property => property.bedrooms)))];
-    const bathrooms = [...new Set(necessaryProperties.map((property => property.bathrooms)))];
-    const locations = [...new Set(necessaryProperties.map((property => property.location[1])))];
-    // TODO: price data view: 100 000 or 100000?
-    const price = [...new Set(necessaryProperties.map((property => property.price.split(" ").join(""))))];
-    options.type = types;
-    options.minArea = Math.min(...area);
-    options.maxArea = Math.max(...area);
-    options.status = statuses;
-    options.bedrooms = bedrooms;
-    options.bathrooms = bathrooms;
-    options.location = locations;
-    options.minPrice = Math.min(...price.map(Number));
-    options.maxPrice = Math.max(...price.map(Number));
-  }
-
   render() {
+    const necessaryProperties = properties.map(property => {
+      return {
+        id: property.id,
+        title: property.title,
+        location: property.location,
+        image: property.images[0],
+        description: property.description,
+        type: property.type,
+        mode: property.mode,
+        price: property.price,
+        area: property.area,
+        bedrooms: property.bedrooms,
+        bathrooms: property.bathrooms
+      }
+    });
+
+    const extract = (key) => [
+      ...new Set(necessaryProperties.map((property => property[key])))
+    ]
+
+    const area = extract("area");
+    const price = extract("price").map(str => Number(str.replaceAll(" ", "")));
+
+    const options = {
+      type: extract("type"),
+      minArea: area,
+      maxArea: area,
+      status: extract("mode"),
+      bedrooms: extract("bedrooms"),
+      bathrooms: extract("bathrooms"),
+      location: extract("location").map(loc => loc[1]),
+      minPrice: price,
+      maxPrice: price
+    };
+    //TODO: price and are maybe array with some step?? or Math.ceil to some bigger value
+    console.log(options)
+
     return <Page title="PROPERTIES" hasSidebar>
       <Sidebar />
       <PropertyList
