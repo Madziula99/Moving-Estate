6426c7f0d3a50c8af09e41d84f09a9180a83fe3a
Magdalena Stopka <m.stopka@godeltech.com>
2022-10-14 12:03:57 +0200

    Add filter values to url and set state of selected options from url on mount and update

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,9 +3,46 @@ import { withRouter } from "react-router-dom";
 import { Page } from "../components/Page/Page.jsx";
 import PropertyList from "../components/PropertyList/PropertyList.jsx";
 import { Sidebar } from "../components/Sidebar/Sidebar.jsx";
+import queryString from 'query-string';
 import properties from "../data.json";
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filterOptions: {}
+    }
+  }
+
+  componentDidMount() {
+    console.log("Component Mounted");
+    const options = queryString.parse(this.props.location.search)
+    this.setState({ filterOptions: options});
+  }
+
+  componentDidUpdate(prevprops, _) {
+    console.log("Component Update");
+    if (prevprops.location.search === this.props.location.search) return;
+    console.log("Search params from url changed");
+    const options = queryString.parse(this.props.location.search)
+    this.setState({ filterOptions: options });
+  }
+
+  serializeToUrl(options) {
+    const filterOptions = queryString.stringify(options);
+    this.props.history.push({
+      search: `?${filterOptions}`,
+    });
+  }
+
+  formSubmit(selectedOptions) {
+    this.setState({
+      options: selectedOptions
+    });
+    this.serializeToUrl(selectedOptions);
+  }
+
   render() {
     const necessaryProperties = properties.map(property => {
       return {
@@ -24,6 +61,9 @@ class Index extends React.Component {
     });
 
     return <Page title="PROPERTIES" hasSidebar>
+      <button onClick={() => this.formSubmit({type: "house", bedrooms: 2, bathrooms:3})}>Click me</button>
+      <button onClick={() => this.formSubmit({ type: "apartment", bedrooms: 3, bathrooms: 3 })}>Click me</button>
+      <button onClick={() => this.formSubmit({ type: "house", bedrooms: 2 })}>Click me</button>
       <Sidebar />
       <PropertyList
         defaultView="grid"
