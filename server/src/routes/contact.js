ade98f60f170ac2daa2962bba1cb0d6ff62375ed
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-10-25 09:56:38 +0200

    Handle server error

--- server/src/routes/contact.js
+++ server/src/routes/contact.js
@@ -3,6 +3,8 @@ const { Router } = require("express");
 async function create(req, res) {
   const message = req.body;
 
+  if (!message) return res.status(404).json({ error: "Message is not found" });
+
   res.json({ message });
 }
 
