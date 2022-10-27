6141afb363c69395de698c9a86981e07a24b511c
Yuliya Mironava <y.mironava@godeltech.com>
2022-10-27 13:38:49 +0200

    Change Spinner. svg background to transperent from rgb (255)

--- client/src/components/Agentinfo/AgentInfo.jsx
+++ client/src/components/Agentinfo/AgentInfo.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import Subtitle from "../Subtitle/Subtitle.jsx";
 import { ContactForm } from "../ContactForm/ContactForm.jsx";
 import { Spinner } from "../Spinner/Spinner.jsx";
-import classes from "./AgentInfo.module.css";
+import styles from "./AgentInfo.module.css";
 
 class AgentInfo extends React.Component {
   state = {
-    loading: false,
+    loading: true,
     success: false,
-    contactForm: true
+    contactForm: false
   }
 
   render() {
     return (
       <section>
         <Subtitle>AGENT INFORMATION</Subtitle>
-        <div className={classes.agent_info}>
-          <div className={classes.info}>
-            <img className={classes.agent} src={this.props.photo} alt="agent" />
-              <div className={classes.name_location}>
-                <h3 className={classes.name}>{this.props.name}</h3>
-                <h4 className={classes.location}>{this.props.location}</h4>
+        <div className={styles.agent_info}>
+          <div className={styles.info}>
+            <img className={styles.agent} src={this.props.photo} alt="agent" />
+              <div className={styles.name_location}>
+                <h3 className={styles.name}>{this.props.name}</h3>
+                <h4 className={styles.location}>{this.props.location}</h4>
               </div>
           </div>
-          {this.state.contactForm && <ContactForm onSubmit={this.props.onSubmit} />}
-          {this.state.success && <h2>Thank you!</h2>}
-          {this.state.loading && <Spinner/>}
+          <div className={styles.message}>
+            {this.state.contactForm && <ContactForm onSubmit={this.props.onSubmit} />}
+            {this.state.success && <h2>Thank you!</h2>}
+            {this.state.loading && <Spinner />}
+          </div>
         </div>
       </section>
     );
