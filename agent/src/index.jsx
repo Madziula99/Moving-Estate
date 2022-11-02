2d8e4006c7cfa1697500f1d23675779aca820fdd
Yuliya Mironava <y.mironava@godeltech.com>
2022-11-02 13:42:32 +0100

    Fix comments and rename page

--- agent/src/index.jsx
+++ agent/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import IndexAgent from './pages/IndexAgent';
+import AgentIndex from './pages/AgentIndex.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
       <BrowserRouter>
         <Switch>
           <Route path="/agent">
-            <IndexAgent />
+            <AgentIndex />
           </Route>
           <Route path="/">
             <h3>Go to path="/agent"</h3>
--- agent/src/pages/AgentIndex.jsx
+++ agent/src/pages/AgentIndex.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import { SignIn } from "../components/SignIn/SignIn";
-import './IndexAgent.css';
+import { SignIn } from "../components/SignIn/SignIn.jsx";
 
-class IndexAgent extends React.Component {
+class AgentIndex extends React.Component {
   render() {
     return <>
       <h3>There will all Agent Sign-in Page</h3>
@@ -12,4 +11,4 @@ class IndexAgent extends React.Component {
   }
 }
 
-export default withRouter(IndexAgent);
+export default withRouter(AgentIndex);
