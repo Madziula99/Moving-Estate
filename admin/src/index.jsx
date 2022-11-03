cd0d2adb3f556c900ff568b171382be8cf04d130
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-11-03 16:17:31 +0100

    Connect /login endpoint to google auth api and add session packages

--- admin/src/index.jsx
+++ admin/src/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import AdminIndex from './pages/AdminIndex.jsx';
-import './index.css';
+import AdminIndex from "./pages/AdminIndex.jsx";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
     <React.StrictMode>
       <BrowserRouter>
         <Switch>
-          <Route path="/agent">
+          <Route path="/admin">
             <AdminIndex />
           </Route>
           <Route path="/">
-            <h3>Go to path="/agent"</h3>
+            <h3>Go to path="/admin"</h3>
           </Route>
         </Switch>
       </BrowserRouter>
--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,27 +1,48 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const { Router } = require("express");
+const session = require("express-session");
 
-const GOOGLE_CLIENT_ID = "778039279925-3i4dtm1d7h82i1uq72g8j04dqkqp6ekc.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+const GOOGLE_CLIENT_ID = "778039279925-l3o03a49ocrdm5lsl80ue5c5hhgs2bo6.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
 
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/",
+  callbackURL: "http://localhost:3500/api/login/google/callback",
   },
   function verify(accessToken, refreshToken, profile, done) {
-    console.log("verified")
     return done(null, profile);
   }
 ));
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 module.exports = Router()
-  .get("/auth/google",
+  .use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "bla bla bla"
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .get("/google",
     passport.authenticate("google", { scope : ["profile", "email"] }))
-  .get("/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "/error" }),
-    function(req, res) {
-      // Successful authentication, redirect success.
-      res.redirect("/success");
-    });
+  .get("/google/callback",
+    passport.authenticate("google", {
+      failureRedirect: "/api/login/error",
+      successRedirect: "/api/login/protected"
+    })
+  )
+  .get("/error", (_, res) => res.json({ error: "An error has occurred" }))
+  .get("/protected", isLoggedIn, (_, res) => res.json({ message: "User logged in" }))
