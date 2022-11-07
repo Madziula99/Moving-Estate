d13ee2721ef4b3e3924496d9fec1e116070fbcb0
Yuliya Mironava <y.mironava@godeltech.com>
2022-11-07 14:21:22 +0100

    Try to fix api response

--- admin/src/pages/AdminsProperties.jsx
+++ admin/src/pages/AdminsProperties.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import { Spinner } from "../components/Spinner/Spinner.jsx";
 import { AdminPropertyTable } from "../components/AdminPropertyTable/AdminPropertyTable.jsx";
-import Data from "../data.json"
+//import Data from "../data.json"
 
 const emailToFilter = "adam@example.com"
-const filterByEmailRows = Data.filter(e => e.agent.email === emailToFilter)
+/*const filterByEmailRows = Data.filter(e => e.agent.email === emailToFilter)
 const filteredProperties =filterByEmailRows.map(property => {
   return {
       picture: { avatar: property.images[0] },
@@ -18,36 +18,46 @@ const filteredProperties =filterByEmailRows.map(property => {
       bedrooms: property.bedrooms,
       bathrooms: property.bathrooms
     }
-  });
+  });*/
 
 class AdminsProperties extends React.Component {
    constructor(props) {
     super(props);
 
     this.state = {
-      filteredByAdminProperties: filteredProperties,
+      filteredByAdminProperties: [],
       agentEmail: emailToFilter,
-      agentName: filterByEmailRows[0].agent.name,
-      isCookies: true,
+      agentName: "filterByEmailRows[0].agent.name",
+      isCookies: true,//TODO: delete if it not need after
       isLoading: false,
     }
   }
 
-  /*TODO: have to filter all on server site and delete data.json there
+  //TODO: have to filter all on server site and delete data.json there
   async getAgentsProperties() {
     this.setState({ isLoading: true });
 
-    const urlQueryParams = new URLSearchParams(this.state.selectedOptions).toString();
+    const urlQueryParams = new URLSearchParams({ email: this.state.agentEmail }).toString().replace(".", "%2E");
+    console.log("/api/properties?" + urlQueryParams, "urlQueryParams")
 
-    await fetch("/admin/properties?" + urlQueryParams)
-      .then(r => r.json())
-      .then(({ properties }) => {
+    //await fetch("/api/properties?" + urlQueryParams)
+      await fetch("/api/properties?email=adam%40example%2Ecom")
+        .then(r => {
+          console.log(JSON.stringify(r));
+          r.json()
+        })
+      .then(properties  => {
         this.setState({
           filteredByAdminProperties: properties,
           isLoading: false
         })
+        console.log(properties, "properties");
       });
-  }*/
+  }
+
+  componentDidMount() {
+    this.getAgentsProperties();
+  }
 
   render() {
     const { filteredByAdminProperties, isCookies, isLoading, agentName } = this.state;
