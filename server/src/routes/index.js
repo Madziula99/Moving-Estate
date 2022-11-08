3a7ce46a4398e6545166f5023cd4fc10302cdfea
h.artsiomenka <h.artsiomenka@godeltech.com>
2022-11-08 12:26:09 +0100

    Extract hardcoded data from auth.js to config and auth logic from index.js to auth.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,26 +2,10 @@ const { Router } = require("express");
 const status = require("./status.js");
 const properties = require("./properties.js");
 const contact = require("./contact.js");
-const login = require("./login.js");
-const session = require("express-session");
-const passport = require("passport");
+const auth = require("./auth.js");
 
 module.exports = Router()
-  .use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: "bla bla bla"
-  }))
-  .use(passport.initialize())
-  .use(passport.session())
   .use("/properties", properties)
   .use("/contact", contact)
-  .use("/login", login)
-  .get("/logout", (req, res, next) => {
-    req.logout(function(err) {
-      if (err) return next(err);
-    });
-    req.session.destroy();
-    res.json({ message: "User logged out" });
-  })
+  .use("/auth", auth)
   .get("/status", status);
