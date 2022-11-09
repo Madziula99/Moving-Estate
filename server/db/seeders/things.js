e88801897fcd0952e0a244d33755df756a2884e5
Ilya Dovban <i.dovban@godeltech.com>
2022-11-09 16:29:06 +0300

    Added Thing model as an example

---
+++ server/db/seeders/things.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const createdAt = new Date();
+    const updatedAt = new Date();
+
+    queryInterface.bulkInsert('Things', [
+      { name: 'Ball', color: 'red', size: 1, createdAt, updatedAt },
+      { name: 'Box', color: 'gray', createdAt, updatedAt },
+      { name: 'Toy Train', size: 3, createdAt, updatedAt }
+    ]);
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    queryInterface.bulkDelete('Things', null);
+  }
+};
