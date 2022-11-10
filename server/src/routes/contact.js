4b5b9a0cbf98bc99d1fa74a5fdc2191f33367992
Magdalena Stopka <m.stopka@godeltech.com>
2022-11-10 11:55:29 +0100

    Add logic on server to create Message record in db on client form submit

--- server/src/routes/contact.js
+++ server/src/routes/contact.js
@@ -2,9 +2,24 @@ const { Router } = require("express");
 const nodeMailer = require("nodemailer");
 const config = require("config");
 const MAIL = config.get("mail");
+const db = require("../models")
 
 async function create(req, res) {
   const { clientName, clientEmail, clientMessage, agentEmail, propertyId } = req.body;
+
+  const createdAt = new Date();
+  const updatedAt = new Date();
+
+  db.Message.create({
+    client_name: clientName,
+    client_email: clientEmail,
+    message: clientMessage,
+    property_id: propertyId,
+    createdAt,
+    updatedAt
+  })
+
+  //db.Message.findAll().then(messages => messages.map(message => console.log(message.get())))
 
   const transporter = nodeMailer.createTransport(MAIL);
 
