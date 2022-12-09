20b055ba179138ee18fc81cfd9ef4af68eeb3b7c
Viktar Astrouski <v.astrouski@godeltech.com>
2022-12-09 15:57:49 +0100

    Add dropdown

--- admin/src/components/FeatureForm/FeatureForm.jsx
+++ admin/src/components/FeatureForm/FeatureForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Button, Input } from "@mui/material";
+import { Button, Input, InputLabel, MenuItem, FormHelperText, FormControl } from "@mui/material";
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import styles from "./FeatureForm.module.css";
 
 class FeatureForm extends React.Component {
@@ -27,11 +28,19 @@ class FeatureForm extends React.Component {
     this.setState({ currentLink: currentLink });
   }
 
+  handleDropdownChange(event) {
+    console.log(event.target.value)
+  };
+
   onSave() {
     const { link, handleSubmit } = this.props;
     const { currentLink } = this.state;
 
     if (currentLink !== link) handleSubmit(currentLink);
+  }
+
+  SelectLabels() {
+    
   }
 
   render() {
@@ -40,6 +49,19 @@ class FeatureForm extends React.Component {
 
     return <div className={styles.overlay}>
       <form className={styles.form}>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          value={1}
+          onChange={this.handleDropdownChange}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem value="paw">Paw</MenuItem>
+          <MenuItem value="pool">Pool</MenuItem>
+          <MenuItem value="fence">Fence</MenuItem>
+        </Select>
+        <FormHelperText>Without label</FormHelperText>
+      </FormControl>
         <Input defaultValue={link} onChange={e => this.handleChange(e.target.value)} autoFocus className={styles.input} />
         <NavLink to={`/properties/${propertyId}/features`} className={styles.nav_link}>
           <Button variant="contained" onClick={this.onSave} disabled={isDisabled} className={styles.row_btn}>Save</Button>
