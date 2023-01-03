d1c21e49f7fece5dd1a7ee0347a3d10af0c907a6
Yuliya Mironava <y.mironava@godeltech.com>
2023-01-03 14:21:07 +0100

    Remove SignOut component

--- admin/src/pages/Messages.jsx
+++ admin/src/pages/Messages.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { withRouter, Redirect } from "react-router-dom";
+import { PageWrapper } from "../components/PageWrapper/PageWrapper.jsx";
 import { PropertyMessages } from "../components/PropertyMessages/PropertyMessages.jsx";
-import { SignOut } from "../components/SignOut/SignOut.jsx";
 import { Spinner } from "../components/Spinner/Spinner.jsx";
 import { Context } from "../Context/Context.js";
+import { MenuButton } from "../controls/MenuButton/MenuButton.jsx";
 
 class Messages extends React.Component {
   state = {
     propertyMessages: [],
     isLoading: true,
     hasAccess: false,
-  }
+  };
 
   getPropertyMessages() {
     const id = this.props.match.params.id;
@@ -19,18 +20,18 @@ class Messages extends React.Component {
 
     const email = this.context.email;
 
-    fetch(`/api/properties/messages/${id}?email=${email}`)
-      .then(res => {
-        if (res.status === 401) {
-          return this.setState({ isLoading: false });
-        } else return res.json().then(messages => {
+    fetch(`/api/properties/messages/${id}?email=${email}`).then((res) => {
+      if (res.status === 401) {
+        return this.setState({ isLoading: false });
+      } else
+        return res.json().then((messages) => {
           this.setState({
             propertyMessages: messages,
             isLoading: false,
-            hasAccess: true
+            hasAccess: true,
           });
         });
-      })
+    });
   }
 
   componentDidMount() {
@@ -41,14 +42,17 @@ class Messages extends React.Component {
     const { propertyMessages, isLoading, hasAccess } = this.state;
     const propertyId = this.props.match.params.id;
 
-    if (isLoading) return <Spinner />
+    if (isLoading) return <Spinner />;
 
-    if (hasAccess) return <>
-      <SignOut headerMessage={`Messages regarding property: ${propertyId}`} />
-      <PropertyMessages propertyMessages={propertyMessages} />
-    </>
+    if (hasAccess)
+      return (
+        <PageWrapper message={`Messages regarding property ${propertyId}`}>
+          <MenuButton text="To properties" href={`/admin/properties`} />
+          <PropertyMessages propertyMessages={propertyMessages} />
+        </PageWrapper>
+      );
 
-    return <Redirect to="/properties" />
+    return <Redirect to="/properties" />;
   }
 }
 
